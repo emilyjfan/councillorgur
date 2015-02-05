db38da7a3a5856abbcdcf17129d077246dd47053
EMILY FAN <emilyjustinefan@gmail.com>
2015-02-04 23:17:17 -0500

    Adding feature to create a new councillor using form_for

--- app/controllers/councillors_controller.rb
+++ app/controllers/councillors_controller.rb
@@ -8,4 +8,19 @@ class CouncillorsController < ApplicationController
 		@councillor = Councillor.find(params[:id])
 	end
 
+	def new
+		@councillor = Councillor.new
+	end
+
+	def create
+		@councillor = Councillor.new(councillor_params)
+		@councillor.save
+		render :text => @councillor
+	end
+
+	private
+  		def councillor_params
+    		params.require(:councillor).permit(:name, :ward, :email, :phone, :website, :picture_url)
+  		end
+
 end
